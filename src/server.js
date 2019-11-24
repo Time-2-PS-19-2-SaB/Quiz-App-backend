c4ff0294683782ba9f8a37c15204cea05389c724
Leandro Gabriel <45115495+VelocityIsntSpeed@users.noreply.github.com>
2019-11-24 14:28:49 -0300

    Habilita CORS

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 // Contém todo o conteúdo de todos os quizzes
 const quizzesContent = require("./quizzesContent");
