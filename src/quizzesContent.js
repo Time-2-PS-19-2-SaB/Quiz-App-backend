160bdc24fc2ff9714194b83761bae5c7b5e601bf
Leandro Gabriel <45115495+VelocityIsntSpeed@users.noreply.github.com>
2019-11-24 03:30:51 -0300

    Adiciona rotas "/quizzes" e "/quizzes/:quizId" e arquivo com conteúdo dos quizzes

---
+++ src/quizzesContent.js
@@ -0,0 +1,53 @@
+// Contém todo o conteúdo de todos os quizzes.
+
+module.exports = {
+  CapitaisDePaises: {
+    name: "Capitais de Países",
+    description: "Teste seu conhecimento das capitais do mundo!",
+    questions: [
+      {
+        questionType: "Text",
+        questionText: "Alemanha",
+        answerType: "Text",
+        rightAnswers: ["Berlim", "Berlin"],
+      },
+      {
+        questionType: "Text",
+        questionText: "Noruega",
+        answerType: "Text",
+        rightAnswers: ["Oslo"],
+      },
+      {
+        questionType: "Text",
+        questionText: "Chile",
+        answerType: "Text",
+        rightAnswers: ["Santiago"],
+      },
+    ],
+  },
+
+  SiglasDeEstados: {
+    name: "Siglas de Estados Brasileiros",
+    description: "Você sabe as siglas de todos os estados do Brasil?",
+    questions: [
+      {
+        questionType: "Text",
+        questionText: "Pernambuco",
+        answerType: "Text",
+        rightAnswers: ["PE"],
+      },
+      {
+        questionType: "Text",
+        questionText: "Rio de Janeiro",
+        answerType: "Text",
+        rightAnswers: ["RJ"],
+      },
+      {
+        questionType: "Text",
+        questionText: "Mato Grosso",
+        answerType: "Text",
+        rightAnswers: ["MT"],
+      },
+    ],
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => res.send("Esta é uma resposta de texto."));
+// Contém todo o conteúdo de todos os quizzes
+const quizzesContent = require("./quizzesContent");
+
+// Retorna todas as informações de todos os quizzes
+app.get("/quizzes", (req, res) => res.json(quizzesContent));
+
+// Retorna as informações de um quiz específico
+app.get("/quizzes/:quizId", (req, res) => {
+  // Verificar se um quiz com esse id existe
+  if (Object.getOwnPropertyNames(quizzesContent).includes(req.params.quizId)) {
+    res.json(quizzesContent[req.params.quizId]);
+  }
+  else {
+    res.status(404).send(`Erro 404: Não existe um quiz com o id "${req.params.quizId}".`);
+  }
+});
 
 app.listen(3333);
